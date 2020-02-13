8ba524f9854c1785f7371ded9463425290ddb2a8
Alex Fryer <afryer@alumni.stanford.edu>
2020-02-13 15:16:08 -0700

    readme and comments

--- README.md
+++ README.md
@@ -1,25 +1,38 @@
-# Getting Started
+# moviedex-api
 
-TODO
+search for movies by genre, country or avg_vote
+** requires authorization header with bearer token **
 
-# Code Overview
+## install
 
-## Requirements
+`npm install`
 
-moviedex-api is an Express app that meets the following specs:
+## run
 
-1. Users can search for Movies by genre, country or avg_vote
-1. The endpoint is GET /movie
-1. The search options for genre, country, and/or average vote are provided in query string parameters.
-1. When searching by genre, users are searching for whether the Movie's genre includes a specified string. The search should be case insensitive.
-1. When searching by country, users are searching for whether the Movie's country includes a specified string. The search should be case insensitive.
-1. When searching by average vote, users are searching for Movies with an avg_vote that is greater than or equal to the supplied number.
-1. The API responds with an array of full movie entries for the search results
-1. The endpoint only responds when given a valid Authorization header with a Bearer API token value.
-1. The endpoint should have general security in place such as best practice headers and support for CORS.
+`npm run start`
 
-## Dependencies
+## run with auto-reload on code change
 
-## Application Structure
+`npm run dev`
 
-## Authentication
+## endpoints
+
+**GET /movie**
+
+Responds with an array of movies, filtered by user params
+
+Example Usage:
+
+  `/movie/?genre=comedy&country=unitedstates&avg_vote=5`
+
+Headers:
+
+  Authorization (Bearer Token, required)
+
+Params:
+
+  ?genre (String, optional)
+  ?country (String, optional)
+  ?avg_score (Number, optional)
+
+
--- server.js
+++ server.js
@@ -1,12 +1,60 @@
+// moviedex-api server
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
+// express app object
 const app = express();
 
+// express middleware setup
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+});
 
-app.use((req, res) => {
-  res.send('Hello, world!');
+// movie database
+const movies = require('./movies-data-small');
+
+// /movie endpoint
+// params: genre:'', country:'', avg_vote:#
+app.get('/movie', (req, res) => {
+  let response = movies;
+  const { query = {} } = req;
+
+  if (query.genre) {
+    if (typeof query.genre === 'string')
+      response = response.filter(m =>
+        m.genre.toLowerCase().includes(query.genre.toLowerCase())
+      );
+    else return res.status(400).json({ error: 'bad genre' });
+  }
+
+  if (query.country) {
+    if (typeof query.country === 'string')
+      response = response.filter(m =>
+        m.country.toLowerCase().includes(query.country.toLowerCase())
+      );
+    else return res.status(400).json({ error: 'bad country' });
+  }
+
+  if (query.avg_vote) {
+    if (!isNaN(Number(query.avg_vote)))
+      response = response.filter(
+        m => Number(m.avg_vote) >= Number(query.avg_vote)
+      );
+    else return res.status(400).json({ error: 'bad avg_vote' });
+  }
+
+  res.json(response);
 });
 
 const PORT = 8000;
